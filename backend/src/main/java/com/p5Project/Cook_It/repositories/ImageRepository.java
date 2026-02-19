4aedf53ab9b5f38ca982361700dc25702b2d988f
Henrique Azevedo <henriquee.aze.dev@gmail.com>
2026-02-19 19:48:37 -0300

    feat: entities repositories

---
+++ backend/src/main/java/com/p5Project/Cook_It/repositories/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.p5Project.Cook_It.repositories;
+
+import com.p5Project.Cook_It.models.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ImageRepository extends JpaRepository<User, UUID> {
+}
