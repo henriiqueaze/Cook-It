cf94f2a24454a3742da6ab6bf181de647b958ff5
Arthur A. <arthurakilalves@gmail.com>
2026-03-02 19:14:30 -0300

    feat: configuração de rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
+import { AppRoutes } from "./routes";
+
 function App() {
-  return (
-    <h1 className="text-3xl font-bold text-green-600">Receitas Inteligentes</h1>
-  );
+  return <AppRoutes />;
 }
 
 export default App;
